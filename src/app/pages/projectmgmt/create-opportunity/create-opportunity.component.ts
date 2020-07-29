0f3b3b59f364b5d54596231ec649d7bdc0946758
ManojkumarRamalingam <rmanoj89@gmail.com>
2020-07-29 05:07:03 -0700

    Currency type to company fix

--- src/app/pages/projectmgmt/create-opportunity/create-opportunity.component.ts
+++ src/app/pages/projectmgmt/create-opportunity/create-opportunity.component.ts
@@ -339,7 +339,7 @@ export class CreateOpportunityComponent implements OnInit {
   }
 
   onCompanyChange(event) {
-    if (event.target.value.includes(this.PMOP_COMANY_LIST[0])) {
+    if (event.target.value.includes(this.PMOP_COMANY_LIST[1])) {
       this.formGroup.get('PMOP_REVENUE_TYPE').patchValue('INR');
     }
     else if (event.target.value.includes(this.PMOP_COMANY_LIST[1])) {
